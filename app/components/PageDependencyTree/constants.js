f5236ba994e74d003b4053441208547cc63e844d
Cameron Yick <cameron.yick@gmail.com>
2019-09-15 22:39:54 -0400

    Tune colors on title bar + nodes

--- app/components/PageDependencyTree/constants.js
+++ app/components/PageDependencyTree/constants.js
@@ -4,10 +4,15 @@ import green from '@material-ui/core/colors/green';
 import grey from '@material-ui/core/colors/grey';
 
 
+// https://tachyons.io/docs/themes/skins/
 export const DEFAULT_NODE_COLOR = blue[100];
-export const ROOT_NODE_COLOR = grey[500];
+// export const DEFAULT_NODE_COLOR = '#96CCFF'; // light-blue
+export const ROOT_NODE_COLOR = grey[600];
 export const SELECTED_NODE_COLOR = red[100];
-export const SELECTED_NODE_OUTLINE = red[500];
+// export const SELECTED_NODE_OUTLINE = red[500];
+export const SELECTED_NODE_OUTLINE = '#E7040F'; // dark red tachyons
 
 export const LEAF_NODE_COLOR = green[100];
-export const ISLAND_NODE_COLOR = grey[200];
+// export const LEAF_NODE_COLOR = '#9EEBCF';
+// export const ISLAND_NODE_COLOR = grey[200];
+export const ISLAND_NODE_COLOR = "#cccccc";
